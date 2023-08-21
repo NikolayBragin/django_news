f72005eeb612f948269bc6f7d8e493cd1236d84d
NikolayBragin <anhksnick@gmail.com>
2023-08-22 01:01:40 +0500

    сайт новостей + правка/удаление

--- django_news/index/models.py
+++ django_news/index/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=512)
     product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product_des = models.TextField(blank=True)
-    product_photo = models.ImageField()
+    product_photo = models.ImageField(upload_to='index\static\img')
 
     def __str__(self):
         return str(self.product_name)
